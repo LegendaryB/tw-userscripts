405ce5ac1ea2e6a051883f89c1a3e716325bccf4
LegendaryB <6391022+LegendaryB@users.noreply.github.com>
2023-01-25 13:22:15 +0100

    Bugfix for the BetterPlayerInfo script. Now also works for own profile.

--- src/userscripts/BetterPlayerInfo/src/modules/FarmAndRZStats.ts
+++ src/userscripts/BetterPlayerInfo/src/modules/FarmAndRZStats.ts
@@ -1,11 +1,28 @@
-import { PlayerInfoTableElement } from "tw-framework";
+import { getGameData, PlayerInfoTableElement } from "tw-framework";
 
 import { getInADayInfo } from "../../../shared";
 import { FarmAndRZTemplate } from "../templates/FarmAndRZTemplate";
 
+interface InfoPlayer {
+    player_id: number;
+}
+
+declare global {
+    interface Window {
+        InfoPlayer: InfoPlayer;
+    }
+}
+
+const InfoPlayer = window.InfoPlayer as InfoPlayer;
+const game_data = getGameData();
+
 export class FarmAndRZStats {
     public static async attach(playerInfoTable: PlayerInfoTableElement) {
         let playerName = document.querySelector('h2').innerText;
+
+        if (InfoPlayer.player_id == game_data.player.player_id) {
+            playerName = game_data.player.name;
+        }
 
         let farm = await getInADayInfo(playerName, 'loot_res');
         let rz = await getInADayInfo(playerName, 'scavenge');
